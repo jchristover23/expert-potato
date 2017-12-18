10ec4a29bb6cfb4da0f87c66bb23f1b272a21c8d
jchristover23 <31273197+jchristover23@users.noreply.github.com>
2017-12-18 21:21:56 +0700

    REV 5 Admin_Login_Page.py
    Color added
    Logo added
    Name added

--- Admin_Login_Page.py
+++ Admin_Login_Page.py
@@ -8,8 +8,9 @@ admin_login = Tk()
 
 admin_login.title("Admin Login")
 admin_login.minsize(width=200, height=200)
-admin_login.maxsize(width=750, height=750)
-admin_login.resizable(width=False, height=False)
+admin_login.maxsize(width=1000, height=750)
+admin_login.resizable(width=FALSE, height=FALSE)
+admin_login.config(bg='white')
 
 def login_success():
     admin_login.destroy()
@@ -17,7 +18,6 @@ def login_success():
 
 def login_failed():
     messagebox.showerror("Alert", "Login Failed! Please Try Again!")
-
 
 def show():
     uname_clean = []
@@ -51,20 +51,18 @@ def show():
     E1.delete(0, END)
     E2.delete(0, END)
 
-
 ADMIN = Label(admin_login, text = "WELCOME! PLEASE LOGIN FIRST.\nBINUSIAN Parking System")
-ADMIN.config(font=("Helvetica", 16))
+ADMIN.config(font=("Helvetica", 16), bg = 'white')
 ADMIN.grid(row = 1, column = 1, pady = 10, padx = 10, sticky = W)
 
 L1 = Label(admin_login, text="Username")
 L1.grid(row = 3, column = 1, pady = 1, padx = 10, sticky = W)
-L1.config(font=("Helvetica", 15))
+L1.config(font=("Helvetica", 15), bg = 'white')
 E1 = Entry(admin_login, font=("Helvetica, 20"))
 E1.grid(row = 4, column = 1, pady = 1, padx = 10, sticky = W, ipady = 5)
 
-
 L2 = Label(admin_login, text="Password")
-L2.config(font=("Helvetica", 15))
+L2.config(font=("Helvetica", 15), bg = 'white')
 L2.grid(row = 5, column = 1, pady = 1, padx = 10, sticky = W)
 E2 = Entry(admin_login, font=("Helvetica, 20"), show = "•")
 E2.grid(row = 6, column = 1, pady = 1, padx = 10, sticky = W, ipady = 5)
@@ -73,11 +71,11 @@ login = Button(admin_login, text = "LOGIN", font=("Helvetica", 20, "bold"), comm
 login.grid(row = 7, column = 2,pady=5, padx = 10, sticky = W)
 
 register = Button(admin_login, text = "REGISTER",font=("Helvetica", 14, "bold"), command = admin_register)
-register.grid(row = 8, column = 2,pady = 5, padx = 10, sticky = W)
+register.grid(row = 8, column = 2, pady = 5, padx = 10, sticky = W)
 
 
 time1 = ''
-clock = Label(admin_login, font=('Helvetica', 25, 'bold'))
+clock = Label(admin_login, font=('Helvetica', 25, 'bold'), bg = 'white')
 clock.grid(row = 9, column = 0)
 def liveclock():
     global time1, time2
@@ -88,6 +86,19 @@ def liveclock():
     clock.after(200, liveclock)
 liveclock()
 
+
 admin_login.bind('<Return>', lambda press_key: show())
+
+
+developed_by = "David Lioner Wijaya - 2101717132 \n Jeremy Christover Gunawan - 2101719434 \n Kenji Surya Utama - 2101717145"
+canvas = Canvas(admin_login, width=300, height=215, bg='white', highlightthickness=0, relief = 'ridge')
+canvas.grid(row = 0, column = 1)
+
+logo = PhotoImage(file='binus.gif')
+canvas.create_image(75, 15, image=logo, anchor=NW)
+
+name = Label(canvas, text=developed_by, fg='black', bg='white')
+name.grid(row = 2, column = 3)
+canvas.create_window(150, 175, window=name)
 
 admin_login.mainloop()
