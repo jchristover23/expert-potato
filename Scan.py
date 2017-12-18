8cbf7de0ba692ee40dbb47824f4f2e48743108ad
jchristover23 <31273197+jchristover23@users.noreply.github.com>
2017-12-18 21:24:15 +0700

    REV 5 Scan.py
    Shift focus added

--- Scan.py
+++ Scan.py
@@ -3,7 +3,6 @@ from Admin_Register_Page import *
 from Karcis import *
 import os
 
-
 def destroyscan():
     scan_id.destroy()
     karcis()
@@ -14,7 +13,9 @@ def keluar():
 
 def scan():
     global scan_id
+
     scan_id = Tk()
+    scan_id.after(1, lambda: scan_id.focus_force())
     scan_id.title("Silahkan Scan Binusian Card Anda")
     scan = Button(scan_id, text = "SCAN", command = destroyscan,height = 5, width = 15 )
     scan.grid(row = 1, column = 1,pady =70, padx = 70)
@@ -24,6 +25,3 @@ def scan():
     exit_button.grid(row=2, column=1, pady=70, padx=70)
     exit_button.config(font=("Helvetica", 15))
     scan_id.mainloop()
-
-
-
